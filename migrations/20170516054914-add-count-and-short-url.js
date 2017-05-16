a7c8031090c4b7283eebdbb142ed8aa9ef963fdd
hakiemaul <noersedya@gmail.com>
2017-05-16 14:22:59 +0700

    added counting and shortened url column

---
+++ migrations/20170516054914-add-count-and-short-url.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return [
+      queryInterface.addColumn(
+        'Urls',
+        'click_count',
+        {
+          type: Sequelize.INTEGER
+        }
+      ),
+      queryInterface.addColumn(
+        'Urls',
+        'url_short',
+        {
+          type: Sequelize.STRING
+        }
+      )
+    ];
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
